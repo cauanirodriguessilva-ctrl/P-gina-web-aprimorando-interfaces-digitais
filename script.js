ee0013cab759cf36c61d0f6e1bc13fbed6f50329
cauanirodriguessilva-ctrl <cauani.rodrigues.silva@escola.pr.gov.br>
2026-08-31 15:21:18 -0300

    paginaweb

---
+++ script.js
@@ -0,0 +1,30 @@
+<script>
+    let likes 0;
+    let isLiked = false;
+
+    function formatNumber(num) {
+        return num.toLocaleString('pt-BR');
+    }
+
+    function toggleLike() {
+       const likeBtn = document.getElementById('likeBtn');
+
+        if (!isLiked) {
+            likes++;
+            isLiked = true;
+            likeBtn.classList.add('liked');
+            likeBtn.innerHTML = `💚 Curtido! <span id="likeCount">${formatNumber(likes)}</span>`;
+        } else {
+            likes--;
+            isLiked = false;
+            likeBtn.classList.remove('liked');
+            likeBtn.innerHTML = `💜 Curtir <span id="likeCount">${formatNumber(likes)}</span>`;
+        }
+
+
+
+</body>
+
+</html>
+    
+
